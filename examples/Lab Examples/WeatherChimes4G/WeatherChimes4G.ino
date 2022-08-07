ba53ea4b10b4a38cf230fc5f533e610b7a6556ca
Will Richards <richawil@oregonstate.edu>
2022-08-07 11:07:37 -0700

    4G WeatherChimes example

---
+++ examples/Lab Examples/WeatherChimes4G/WeatherChimes4G.ino
@@ -0,0 +1,85 @@
+/**
+ * In lab use case example for the WeatherChimes project
+ * 
+ * This project uses SDI12, TSL2591 and an SHT31 sensor to log environment data and logs it to both the SD card and also MQTT/MongoDB
+ * 
+ * MANAGER MUST BE INCLUDED FIRST IN ALL CODE
+ */
+#include "arduino_secrets.h"
+
+#include <Loom_Manager.h>
+
+#include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
+
+#include <Sensors/Loom_Analog/Loom_Analog.h>
+#include <Sensors/SDI12/Loom_SDI12/Loom_SDI12.h>
+#include <Sensors/I2C/Loom_SHT31/Loom_SHT31.h>
+#include <Sensors/I2C/Loom_TSL2591/Loom_TSL2591.h>
+
+#include <Internet/Connectivity/Loom_Wifi/Loom_Wifi.h>
+#include <Internet/Logging/Loom_MQTT/Loom_MQTT.h>
+
+Manager manager("Chime", 1);
+
+// Analog for reading battery voltage
+Loom_Analog analog(manager);
+
+// Create a new Hypnos object
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_2, TIME_ZONE::PST);
+
+// Create the TSL2591 and SHT classes
+Loom_SHT31 sht(manager);
+Loom_TSL2591 tsl(manager);
+Loom_SDI12 sdi(manager, 11);
+
+Loom_LTE lte(manager, NETWORK_NAME, NETWORK_USER, NETWORK_PASS);
+Loom_MQTT mqtt(manager, lte.getClient(), SECRET_BROKER, SECRET_PORT, DATABASE, BROKER_USER, BROKER_PASS);
+
+// Called when the interrupt is triggered 
+void isrTrigger(){
+  hypnos.wakeup();
+}
+
+void setup() {
+
+  // Wait 20 seconds for the serial console to open
+  manager.beginSerial();
+
+  // Enable the hypnos rails
+  hypnos.enable();
+
+  // Load the WiFi login credentials from a file on the SD card
+  wifi.loadConfigFromJSON(hypnos.readFile("wifi_creds.json"));
+
+  // Initialize all in-use modules
+  manager.initialize();
+
+
+  // Register the ISR and attach to the interrupt
+  hypnos.registerInterrupt(isrTrigger);
+}
+
+void loop() {
+
+  // Measure and package the data
+  manager.measure();
+  manager.package();
+  
+  // Print the current JSON packet
+  manager.display_data();            
+
+  // Log the data to the SD card              
+  hypnos.logToSD();
+
+  // Publish the collected data to MQTT
+  mqtt.publish();
+
+  // Set the RTC interrupt alarm to wake the device in 10 seconds
+  hypnos.setInterruptDuration(TimeSpan(0, 0, 0, 10));
+
+  // Reattach to the interrupt after we have set the alarm so we can have repeat triggers
+  hypnos.reattachRTCInterrupt();
+  
+  // Put the device into a deep sleep, operation HALTS here until the interrupt is triggered
+  hypnos.sleep(false);
+}
---
+++ examples/Lab Examples/WeatherChimes4G/arduino_secrets.h
@@ -0,0 +1,9 @@
+#define NETWORK_NAME ""
+#define NETWORK_USER ""
+#define NETWORK_PASS ""
+
+#define SECRET_BROKER ""
+#define SECRET_PORT 1883
+#define DATABASE "WeatherChimesV4"
+#define BROKER_USER "User"
+#define BROKER_PASS "password"
