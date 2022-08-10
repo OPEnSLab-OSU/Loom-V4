4409efc63a31a1d113dc9f45b9bdd956a17e2664
Will Richards <richawil@oregonstate.edu>
2022-08-10 10:01:50 -0700

    added option for display_data in module

--- src/Module.h
+++ src/Module.h
@@ -18,12 +18,15 @@ class Module{
         virtual void printModuleName() { Serial.print("[" + String(getModuleName()) + "] "); };
 
         // Generic measure and package calls to unify some interaction with different sensor implementations
-        virtual void initialize() = 0;
-        virtual void measure() = 0;
-        virtual void package() = 0;
-        virtual void print_measurements() = 0;
-        virtual void power_up() = 0;
-        virtual void power_down() = 0;
+        virtual void initialize() = 0;                      // Initialize all functionality of the sensor
+        virtual void measure() = 0;                         // Collect data from the sensor
+        virtual void package() = 0;                         // Package collected data into JSON document
+        virtual void print_measurements() = 0;              // Print the measurements from that sensor to the serial monitor
+        virtual void power_up() = 0;                        // Power the sensor up and come out of sleep
+        virtual void power_down() = 0;                      // Power the sensor down to prepare for sleep
+
+        // Not required overrides
+        virtual void display_data() {};                     // Called by the manager to allow OLED to display data at the same time as manager.display_data  
 
         bool moduleInitialized = true;
     private:
