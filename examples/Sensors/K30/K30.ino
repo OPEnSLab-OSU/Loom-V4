0645ea0d593fb6bc1ef6c343bda91828fb42e797
Will Richards <richawil@oregonstate.edu>
2022-10-23 12:34:52 -0700

    Updated K30 example to only have I2C

--- examples/Sensors/K30/K30.ino
+++ examples/Sensors/K30/K30.ino
@@ -13,22 +13,12 @@ Manager manager("Device", 1);
 
 // Reads the battery voltage
 // Manger Instance, Address Enable Warmup, Value Multiplier
-//Loom_K30 k30(manager, false, 0x68, true, 1);
-Uart kSerial(&sercom1, 12, 11, SERCOM_RX_PAD_3, UART_TX_PAD_0);
-
-// Serial Interface for the K30
-Loom_K30 k30(manager, 12, 11, true, 1);
+Loom_K30 k30(manager, false, 0x68, true, 1);
 
 void setup() {
 
   // Start the serial interface and wait for the user to open the serial monitor
   manager.beginSerial();
-
-  //Assign pins 10 & 11 SERCOM functionality
-  pinPeripheral(11, PIO_SERCOM);
-  pinPeripheral(12, PIO_SERCOM);
-
-  k30.setSerial(kSerial);
 
   // Initialize the manager
   manager.initialize();
@@ -47,8 +37,4 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-}
-
-void SERCOM1_Handler(){
-  kSerial.IrqHandler();
 }
