1dbd24ec60aec2e071ddc66087c44da52158c742
solidstatebird <33707046+solidstatebird@users.noreply.github.com>
2023-03-18 15:17:08 -0700

    Add colorType constructor parameter to NeoPixel

--- src/Hardware/Actuators/Loom_Neopixel/Loom_Neopixel.cpp
+++ src/Hardware/Actuators/Loom_Neopixel/Loom_Neopixel.cpp
@@ -2,29 +2,29 @@
 #include "Logger.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-Loom_Neopixel::Loom_Neopixel(Manager& man, const bool enableA0, const bool enableA1, const bool enableA2) : 
+Loom_Neopixel::Loom_Neopixel(Manager& man, const bool enableA0, const bool enableA1, const bool enableA2, const neoPixelType colorType) : 
     Actuator(ACTUATOR_TYPE::NEOPIXEL, 0), 
     manInst(&man), 
     enabledPins{ enableA0, enableA1, enableA2 },
-	pixels{ Adafruit_NeoPixel(1, 14, NEO_RGB + NEO_KHZ800),
-            Adafruit_NeoPixel(1, 15, NEO_RGB + NEO_KHZ800),
-            Adafruit_NeoPixel(1, 16, NEO_RGB + NEO_KHZ800) }
+    pixels{ Adafruit_NeoPixel(1, 14, colorType + NEO_KHZ800),
+            Adafruit_NeoPixel(1, 15, colorType + NEO_KHZ800),
+            Adafruit_NeoPixel(1, 16, colorType + NEO_KHZ800) }
 {
     this->enabledPins[0] = enableA0;
     this->enabledPins[1] = enableA1;
     this->enabledPins[2] = enableA2;
 
-	manInst->registerModule(this);
+    manInst->registerModule(this);
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-Loom_Neopixel::Loom_Neopixel(const bool enableA0, const bool enableA1, const bool enableA2) : 
+Loom_Neopixel::Loom_Neopixel(const bool enableA0, const bool enableA1, const bool enableA2, const neoPixelType colorType) : 
     Actuator(ACTUATOR_TYPE::NEOPIXEL, 0), 
     enabledPins{ enableA0, enableA1, enableA2 },
-	pixels{ Adafruit_NeoPixel(1, 14, NEO_RGB + NEO_KHZ800),
-            Adafruit_NeoPixel(1, 15, NEO_RGB + NEO_KHZ800),
-            Adafruit_NeoPixel(1, 16, NEO_RGB + NEO_KHZ800) }
+    pixels{ Adafruit_NeoPixel(1, 14, colorType + NEO_KHZ800),
+            Adafruit_NeoPixel(1, 15, colorType + NEO_KHZ800),
+            Adafruit_NeoPixel(1, 16, colorType + NEO_KHZ800) }
 {
     this->enabledPins[0] = enableA0;
     this->enabledPins[1] = enableA1;
@@ -36,17 +36,17 @@ Loom_Neopixel::Loom_Neopixel(const bool enableA0, const bool enableA1, const boo
 void Loom_Neopixel::initialize(){
     FUNCTION_START;
     // Set pin mode on enabled pins (pins A0-A5 = 14-19)
-	for (int i = 0; i < 3; i++) {
-		if (enabledPins[i]) pinMode(14+i, OUTPUT);
-	}
+    for (int i = 0; i < 3; i++) {
+        if (enabledPins[i]) pinMode(14+i, OUTPUT);
+    }
 
     // Initialize Neopixels
-	for (int i = 0; i < 3; i++) {
-		if (enabledPins[i]) {
-			pixels[i].begin(); // This initializes the NeoPixel library.
-			pixels[i].show();  // Initialize all pixels to 'off'
-		}
-	}
+    for (int i = 0; i < 3; i++) {
+        if (enabledPins[i]) {
+            pixels[i].begin(); // This initializes the NeoPixel library.
+            pixels[i].show();  // Initialize all pixels to 'off'
+        }
+    }
 
     LOG(F("Successfully initialized Neopixel"));
     FUNCTION_END;
@@ -71,16 +71,16 @@ void Loom_Neopixel::set_color(const uint8_t port, const uint8_t chain_num, const
     FUNCTION_START;
     char output[OUTPUT_SIZE];
     if ( enabledPins[port] ) {
-		// Apply color
-		pixels[port].setPixelColor(chain_num, pixels[port].Color(red, green, blue));
+        // Apply color
+        pixels[port].setPixelColor(chain_num, pixels[port].Color(red, green, blue));
 
-		// Update colors displayed by Neopixel
-		pixels[port].show();
-		
-	} else {
+        // Update colors displayed by Neopixel
+        pixels[port].show();
+        
+    } else {
         snprintf(output, OUTPUT_SIZE, "Neopixel not enabled on port %u", port);
-		WARNING(output);
-	}
+        WARNING(output);
+    }
     FUNCTION_END;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,11 +90,11 @@ void Loom_Neopixel::enable_pin(const uint8_t port, const bool state){
     FUNCTION_START;
     char output[OUTPUT_SIZE];
     enabledPins[port] = state;
-	if (state) {
-		pinMode(port, OUTPUT);
-	}
+    if (state) {
+        pinMode(port, OUTPUT);
+    }
     snprintf(output, OUTPUT_SIZE, "Neopixel state changed on port %u", port);
-	LOG(output);
+    LOG(output);
     FUNCTION_END;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
