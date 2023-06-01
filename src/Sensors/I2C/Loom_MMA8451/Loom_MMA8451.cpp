283aec1481b8c9f51c15f27026a9f8a4d93ddfc0
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2023-06-01 10:31:47 -0700

    Update Loom_MMA8451.cpp, returing x,y,z

--- src/Sensors/I2C/Loom_MMA8451/Loom_MMA8451.cpp
+++ src/Sensors/I2C/Loom_MMA8451/Loom_MMA8451.cpp
@@ -96,7 +96,9 @@ void Loom_MMA8451::package() {
     if(moduleInitialized){
         JsonObject json = manInst->get_data_object(getModuleName());
         json["X_Acc"] = accel[2];
-
+        json["Y_Acc"] = accel[0];
+        json["Z_Acc"] = accel[1];
+        
         switch (orientation) {
             case MMA8451_PL_PUF: strncpy(orientationString, "Portrait_Up_Front\0", 25);       break;
             case MMA8451_PL_PUB: strncpy(orientationString, "Portrait_Up_Back\0" , 25);		break;
