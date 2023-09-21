47441eb1d71a317bd709f1ca354e2e3823827e50
Will Richards <richawil@oregonstate.edu>
2023-09-20 18:46:37 -0700

    Add publishMessage and deleteRetained to RemoteManager

--- src/Internet/Communication/Loom_RemoteManager/Loom_RemoteManager.cpp
+++ src/Internet/Communication/Loom_RemoteManager/Loom_RemoteManager.cpp
@@ -1,7 +1,118 @@
 #include "Loom_RemoteManager.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-Loom_RemoteManager::Loom_RemoteManager(Manager& man, ) : manInst(&man), Module("RemoteManager") {}
+Loom_RemoteManager::Loom_RemoteManager(
+                Manager& man, 
+                Client& internet_client, 
+                const char* broker_address, 
+                int broker_port, 
+                const char* database_name, 
+                const char* broker_user, 
+                const char* broker_pass,
+            ) : manInst(&man), Module("RemoteManager"), port(broker_port), internetClient(&internet_client), mqttClient(*internetClient){
+                strncpy(this->address, broker_address, 100);
+                strncpy(this->username, broker_user, 100);
+                strncpy(this->password, password, 100);
+            }
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+Loom_RemoteManager::Loom_RemoteManager(Manager& man, Client& internet_client) : manInst(&man), Module("RemoteManager") {}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void Loom_RemoteManager::initialize(){
+    power_up();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void Loom_RemoteManager::power_up(){
+    // Connect to the MQTT broker 
+    connectToBroker();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Loom_RemoteManager::connectToBroker(){
+    if(moduleInitialized){
+
+        // If we are logging in using credentials then supply them
+        if(strlen(username) > 0)
+            mqttClient.setUsernamePassword(username, password);
+        
+        int retryAttempts = 0;
+
+        // Try to connect multiple times as some may be dropped
+        while(!mqttClient.connected())
+        {
+            // If our retry limit has been reached we dont want to try to send data cause it wont work
+            if(retryAttempts >= maxRetries){
+                ERROR(F("MQTT Retry limit exceeded!"));
+                TIMER_ENABLE;
+                FUNCTION_END;
+                return false;
+            }
+
+            snprintf_P(output, OUTPUT_SIZE, PSTR("Attempting to connect to broker: %s:%i"), address, port);
+            LOG(output);
+
+            // Attempt to Connect to the MQTT client 
+            if(!mqttClient.connect(address, port)){
+                snprintf_P(output, OUTPUT_SIZE, PSTR("Failed to connect to broker: %s"), getMQTTError());
+                ERROR(output);
+                delay(5000);
+            }
+
+            retryAttempts++;
+        }
+
+        LOG(F("Successfully connected to broker!"));
+
+        // Tell the broker we are still here
+        mqttClient.poll();
+        
+        return true;
+    }
+
+    return false;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Loom_RemoteManager::publishMessage(const char* topic, const char* message){
+    FUNCTION_START;
+    if(moduleInitialized){
+        // Start a message write the data and close the message, publish all messages with retain
+        if(mqttClient.beginMessage(topic, true, 2) != 1){
+            ERROR(F("Failed to begin message!"));
+        }
+
+        // Print the message to the topic
+        mqttClient.println(message);
+
+        // Check to see if we are actually closing messages properly
+        if(mqttClient.endMessage() != 1){
+            ERROR(F("Failed to close message!"));
+            FUNCTION_END;
+            return false;
+        }
+        else{
+            LOG(F("Data has been successfully sent!"));
+            FUNCTION_END;
+            return true;
+        }   
+    }
+    else{
+        ERROR("Module not initialized!");
+    }
+    FUNCTION_END;
+    return false;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Loom_RemoteManager::deleteRetained(const char* topic) { publishMessage(topic, ""); }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -12,31 +123,68 @@ void Loom_RemoteManager::loadConfigFromJSON(char* json){
     StaticJsonDocument<300> doc;
     DeserializationError deserialError = deserializeJson(doc, json);
 
+    /* 
+        Initialize all to null bytes so they are treated as a 0 length string
+        Issue: https://github.com/OPEnSLab-OSU/Loom-V4/issues/57
+     */
+    memset(username, '\0', 100);
+    memset(password, '\0', 100);
+
     // Check if an error occurred and if so print it
     if(deserialError != DeserializationError::Ok){
         snprintf_P(output, OUTPUT_SIZE, PSTR("There was an error reading the MQTT credentials from SD: %s"), deserialError.c_str());
         ERROR(output);
     }
+    
 
     // Only update values if not null
     if(!doc["broker"].isNull()){
         strncpy(address, doc["broker"].as<const char*>(), 100);
-        strncpy(database_name, doc["database"].as<const char*>(), 100);
-        strncpy(username, doc["username"].as<const char*>(), 100);
-        strncpy(password, doc["password"].as<const char*>(), 100);
-        strncpy(projectServer, doc["project"].as<const char*>(), 100);
+
+        // If we dont have a username don't try to update the variables
+        if(!doc["username"].isNull()){
+            strncpy(username, doc["username"].as<const char*>(), 100);
+            strncpy(password, doc["password"].as<const char*>(), 100);
+        }
+       
         port = doc["port"].as<int>();
     }
-    
-    if(strlen(projectServer) > 0)
-            // Formulate a topic to publish on with the format "ProjectName/DatabaseName/DeviceNameInstanceNumber" eg. WeatherChimes/Chimes/Chime1
-            snprintf_P(topic, 100, PSTR("%s/%s/%s%i"), projectServer, database_name, manInst->get_device_name(), manInst->get_instance_num());
-        else
-            // Formulate a topic to publish on with the format "DatabaseName/DeviceNameInstanceNumber" eg. WeatherChimes/Chime1
-            snprintf_P(topic, 100, PSTR("%s/%s%i"), database_name, manInst->get_device_name(), manInst->get_instance_num());
-    
+
     moduleInitialized = true;
     free(json);
     FUNCTION_END;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void Loom_RemoteManager::getMQTTError(){
+    // Convert error codes to actual descriptions
+    FUNCTION_START;
+    switch(mqttClient.connectError()){
+        case -2:
+            FUNCTION_END;
+            return "CONNECTION_REFUSED";
+        case -1:
+            FUNCTION_END;
+            return "CONNECTION_TIMEOUT";
+        case 1:
+            FUNCTION_END;
+            return "UNACCEPTABLE_PROTOCOL_VERSION";
+        case 2:
+            FUNCTION_END;
+            return "IDENTIFIER_REJECTED";
+        case 3:
+            FUNCTION_END;
+            return "SERVER_UNAVAILABLE";
+        case 4:
+            FUNCTION_END;
+            return "BAD_USER_NAME_OR_PASSWORD";
+        case 5:
+            FUNCTION_END;
+            return "NOT_AUTHORIZED";
+        default:
+            FUNCTION_END;
+            return "UNKNOWN_ERROR";
+    }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/Internet/Communication/Loom_RemoteManager/Loom_RemoteManager.h
+++ src/Internet/Communication/Loom_RemoteManager/Loom_RemoteManager.h
@@ -15,10 +15,12 @@ class Loom_RemoteManager : public Module{
     protected:
          /* These aren't used with the RemoteManager yet */
         void measure() override {};                               
-        void initialize() override {};    
-        void power_up() override {};
         void power_down() override {}; 
         void package() override {};
+
+        /* Used with the manager */
+        void initialize() override;   
+        void power_up() override;
 
     public:
 
@@ -26,9 +28,28 @@ class Loom_RemoteManager : public Module{
          * Remote Manager constructor
          * 
          * @param man Reference to the Loom manager
+         * @param internet_client Reference to a internet Client object
+         * @param broker_address Address of the MQTT broker we are going to use
+         * @param broker_port Broker connection port
+         * @param broker_user Username to connect to the broker 
+         * @param broker_pass Password to connect to the broker
         */
-        Loom_RemoteManager(Manager& man, );
+        Loom_RemoteManager(
+                Manager& man, 
+                Client& internet_client, 
+                const char* broker_address, 
+                int broker_port, 
+                const char* broker_user = "", 
+                const char* broker_pass = "",
+            );
         
+        /**
+         * Barebones Constructor you will need to use loadConfigFromJSON to set the credentials
+         * 
+         * @param man Reference to the manager
+         * @param internet_client Reference to the internet Client object
+        */
+        Loom_RemoteManager(Manager& man, Client& internet_client);
 
         /**
          * Load the MQTT credentials from a JSON string, used to pull credentials from a file
@@ -37,8 +58,23 @@ class Loom_RemoteManager : public Module{
         void loadConfigFromJSON(char* json);
 
     private:
-        Manager manInst = nullptr;          // Instance of the Loom Manager
+        Manager manInst = nullptr;                                      // Instance of the Loom Manager
 
-        Client* internetClient;                  // Client to supply to the MQTT client to handle internet communication
-        MqttClient mqttClient;                  // MQTT Client to manage interactions with the MQTT broker
+        Client* internetClient;                                         // Client to supply to the MQTT client to handle internet communication
+        MqttClient mqttClient;                                          // MQTT Client to manage interactions with the MQTT broker
+
+        bool connectToBroker();                                         // Connect to the MQTT broker
+        const char* getMQTTError();                                     // Convert the MQTT error code into a string
+
+        /* Publish a given message to a given topic with retain set to true */
+        bool publishMessage(const char* topic, const char* message);    // Publish a given message to a specific topic
+        bool deleteRetained(const char* topic);                         // Delete the last retained message
+
+        /* MQTT Parameters */
+        int maxRetries = 4;                                             // How many times we want to retry the connection
+        char address[100];                                              // Domain that the broker is running on
+        int port;                                                       // Port the broker is listening on
+        char topic[100];                                                // Where to publish the data to
+        char username[100];                                             // Username to log into the broker
+        char password[100];                                             // Password to log into the broker
 };
