27f3d7e56b91fcbfa0399ea55b83f1e47eb87623
ZakaryW <wattsz>
2024-01-26 12:19:06 -0800

    Modified the disable and enable calls made in sleep to handle the specific flags set for the 3.3v and 5v rails

--- src/Hardware/Loom_Hypnos/Loom_Hypnos.h
+++ src/Hardware/Loom_Hypnos/Loom_Hypnos.h
@@ -18,7 +18,7 @@ using InterruptCallbackFunction = void (*)();
 
 /**
  * Tracks the hypnos version and matches the version with the correct chip select pin
- */ 
+ */
 enum HYPNOS_VERSION{
     V3_2 = 10,
     V3_3 = 11,
@@ -27,7 +27,7 @@ enum HYPNOS_VERSION{
 
 /**
  * Time zone abbreviations that map to the hour offset from UTC
- */ 
+ */
 enum TIME_ZONE{
     WAT = 1,
     AT = 2,
@@ -57,7 +57,7 @@ enum TIME_ZONE{
 
 /**
  * Type of interrupt to register
- */ 
+ */
 enum HypnosInterruptType{
     SLEEP,
     OTHER
@@ -65,22 +65,22 @@ enum HypnosInterruptType{
 
 /**
  * All in one driver for the Hypnos board. This allows users to use the Hypnos board in a more modularized manner not requiring all the Loom stuff.
- * 
+ *
  * @author Will Richards
- */ 
+ */
 class Loom_Hypnos : public Module{
     protected:
 
         /* These aren't used with the Hypnos */
-        void measure() override {};                               
-       
-        void initialize() override {};  
-        
+        void measure() override {};
+
+        void initialize() override {};
+
         void power_up() override {};
-        void power_down() override {}; 
+        void power_down() override {};
 
         // We want to use the package method to add the timestamp to the JSON
-        void package() override;                           
+        void package() override;
     public:
 
         volatile bool shouldPowerUp = true;
@@ -92,12 +92,12 @@ class Loom_Hypnos : public Module{
          * @param timezone The current timezone the clock was set to
          * @param use_custom_time Use a specific time set by the user that is different than the compile time
          * @param useSD Whether or not SD card functionality should be enabled
-         */ 
+         */
         Loom_Hypnos(Manager& man, HYPNOS_VERSION version, TIME_ZONE zone, bool use_custom_time = true, bool useSD = true);
 
         /**
          *  Cleanup any dynamically allocated pointers
-         */ 
+         */
         ~Loom_Hypnos();
 
         /* Power Control Functionality */
@@ -105,23 +105,23 @@ class Loom_Hypnos : public Module{
         /**
          * Enable the Hypnos board
          * Sets the power rail pins to OUTPUT mode and then enables them
-         * 
+         *
          * @param enable33 whether or not to enable the 3.3v rails
          * @param enable5 whether or not to enable the 5v and 12v rails
-         */ 
+         */
         void enable(bool enable33 = true, bool enable5 = true);
 
         /**
          * Disables the Hypnos Board
          * Disables the Power Rails and sets the SPI pins to INPUT which effectively disables them
-         */ 
+         */
         void disable(bool disable33 = true, bool disable5 = true);
 
         /* SD Functionality */
 
         /**
          * Log the current sensor data to a file on the SD card
-         */ 
+         */
         bool logToSD();
 
         /* Sleep Functionality */
@@ -132,25 +132,25 @@ class Loom_Hypnos : public Module{
          * @param interruptPin Defaults to RTC pin on Hypnos can be changed to reflect other interrupts
          * @param interruptType Type of the interrupt to register (SLEEP or OTHER)
          * @param triggerState When the interrupt should trigger
-         */ 
+         */
         bool registerInterrupt(InterruptCallbackFunction isrFunc = nullptr, int interruptPin = 12, HypnosInterruptType interruptType = SLEEP, int triggerState = LOW);
 
         /**
          * Called when the user wants to wake the Hypnos back out of the sleep state
          * This detaches the interrupt AND re-enables the power rails
-         */ 
+         */
         void wakeup();
 
         /**
          * Called when the user wants to reattach the interrupt handler to the RTC interrupt to collect subsequent interrupts
          * @param interruptPin Pin to reattach the interrupt to for RTC this doesn't need to be changed
-         */ 
+         */
         bool reattachRTCInterrupt(int interruptPin = 12);
 
         /**
          * Set the next interrupt to be triggered at a set interval in the future
          * @param duration The time that will elapse before the next interrupt is triggered
-         */ 
+         */
         void setInterruptDuration(const TimeSpan duration);
 
         /**
@@ -158,55 +158,55 @@ class Loom_Hypnos : public Module{
          * @param waitForSerial Whether or not we should wait for the user to open the serial monitor before continuing execution
 	 * @param disable33 Whether or not to disable 3.3V rails
 	 * @param disable5 Whether or not to disable 5V and 12V rails
-         */ 
+         */
         void sleep(bool waitForSerial = false, bool disable33 = true, bool disable5 = true);
 
         /**
          * Get the current time from the RTC
-         */ 
-        DateTime getCurrentTime(); 
+         */
+        DateTime getCurrentTime();
 
         /**
          * Convert the current time to a ISO 8601 compatible time string
-         * 
+         *
          * @param time The current time as a DateTime object
          * @param array The buffer to write the string to (size 21)
         */
-        void dateTime_toString(DateTime time, char array[21]);                              
+        void dateTime_toString(DateTime time, char array[21]);
 
         /**
          * Set a custom time on startup for the RTC to use
         */
         void set_custom_time();
-    
+
         /**
          * Get a custom sleep interval specified in a file on the SD card
          * If there was an error reading the file the sleep interval will be set to 20 minutes
          * @param fileName Name of the file to pull information from
          * @return TimeSpan with the parsed data
-         */ 
+         */
         TimeSpan getSleepIntervalFromSD(const char* fileName);
 
         /**
          * Get and set the timezone for the Hypnos from the SD card
          * @param fileName Name of the file to pull information from
-         */ 
+         */
         void getTimeZoneFromSD(const char* fileName);
 
         /**
          * Read file from SD
          * @param fileName File to read from
-         */ 
+         */
         char* readFile(const char* fileName) { return sdMan->readFile(fileName); };
 
         /**
-         * Get the default SD card file name 
+         * Get the default SD card file name
          */
         const char* getDefaultFilename(){ return sdMan->getDefaultFilename(); };
 
         /**
          * Get an instance of the SD manager, used for batch SD
-         */ 
+         */
         SDManager* getSDManager() { return sdMan; };
 
         /* Set a network interface in the Hypnos so we can sync our time */
@@ -220,7 +220,7 @@ class Loom_Hypnos : public Module{
 
         /**
          * Set an alternative name to log data to
-         */ 
+         */
         void setLogName(const char* name) { sdMan->setLogName(name); };
 
         /* Return initialization state of the RTC */
@@ -241,15 +241,15 @@ class Loom_Hypnos : public Module{
 
         RTC_DS3231 RTC_DS;                                                                  // Real time clock reference
         bool RTC_initialized = false;                                                       // Did the RTC initialize correctly?
-        
+
         bool custom_time = false;                                                           // Set the RTC to a user specified time
 
         // Map the given pin to an interrupt call back
         // 0th - ISR
         // 1st - Interrupt Trigger
         // 2nd - Interrupt Type (SLEEP or OTHER)
-        std::map<int, std::tuple<InterruptCallbackFunction, int, HypnosInterruptType>> pinToInterrupt;            
-        
+        std::map<int, std::tuple<InterruptCallbackFunction, int, HypnosInterruptType>> pinToInterrupt;
+
         void initializeRTC();                                                               // Initialize RTC
 
         void createTimezoneMap();                                                           // Map Timezone Strings to Timezone enum
@@ -257,17 +257,17 @@ class Loom_Hypnos : public Module{
 
         DateTime get_utc_time();                                                            // Convert the local time to UTC, accounts for daylight savings zones
         TIME_ZONE timezone;                                                                 // Timezone the RTC was set to
-       
-        
+
+
 
         DateTime time;                                                                      // UTC time
         DateTime localTime;                                                                 // Local time
 
         /* Sleep functionality */
 
-        void pre_sleep();                                                                   // Called just before the hypnos enters sleep, this disconnects the power rails and the serial bus
-        void post_sleep(bool waitForSerial);                                                // Called just after the hypnos wakes up, this reconnects the power rails and the serial bus
+        void pre_sleep(bool disable33=true, bool disable5=true);                                                                   // Called just before the hypnos enters sleep, this disconnects the power rails and the serial bus
+        void post_sleep(bool waitForSerial, bool disable33=true, bool disable5=true);                                                // Called just after the hypnos wakes up, this reconnects the power rails and the serial bus
 
-        
+
 
 };
