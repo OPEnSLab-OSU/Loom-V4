bbccc3c273001980adcea8068293e231e0e60abc
Will Richards <jwilt9000@gmail.com>
2025-01-14 15:58:22 -0800

    Moved hypnos log statements to be after all I2C calls

--- src/Hardware/Loom_Hypnos/Loom_Hypnos.cpp
+++ src/Hardware/Loom_Hypnos/Loom_Hypnos.cpp
@@ -471,14 +471,14 @@ void Loom_Hypnos::sleep(bool waitForSerial){
     if(shouldPowerUp){
         manInst->power_down();
 
-        // 50ms delay allows this last message to be sent before the bus disconnects
-        LOG("Entering Standby Sleep...");
-        delay(50);
-
         // After powering down the devices check if the alarmed time is less than the current time, this means that the alarm may have already triggered
         uint32_t alarmedTime = RTC_DS.getAlarm(1).unixtime();
         uint32_t currentTime = RTC_DS.now().unixtime();
         hasAlarmTriggered = alarmedTime <= currentTime;
+        
+        // 50ms delay allows this last message to be sent before the bus disconnects
+        LOG("Entering Standby Sleep...");
+        delay(50);
     }
 
     // If it hasn't we should preform our sleep as before
@@ -538,6 +538,7 @@ void Loom_Hypnos::post_sleep(bool waitForSerial){
         enable(enable33, enable5); // Checks if the 3.3v or 5v are disabled and re-enables them
         delay(1000);
 
+        LOG(F("Device has awoken from sleep!"));
 
         // Clear any pending RTC alarms
         RTC_DS.clearAlarm();
@@ -552,7 +553,7 @@ void Loom_Hypnos::post_sleep(bool waitForSerial){
             TIMER_ENABLE;
         }
 
-        LOG(F("Device has awoken from sleep!"));
+        
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
