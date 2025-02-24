325b1f38a14ce54206d60af746d5b2f4508ed18a
ZakaryW <wattsz>
2025-02-23 17:45:44 -0800

    Debugging changes

--- src/Sensors/I2C/Loom_MultiGasSensor/Loom_MultiGasSensor.cpp
+++ src/Sensors/I2C/Loom_MultiGasSensor/Loom_MultiGasSensor.cpp
@@ -1,4 +1,5 @@
 #include "Loom_MultiGasSensor.h"
+#include "Wire.h"
 #include "Logger.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -6,7 +7,7 @@ Loom_MultiGasSensor::Loom_MultiGasSensor(
                             Manager& man,
                             uint8_t address,
                             bool useMux
-                    ) : I2CDevice("MultiGasSensor"), manInst(&man), addr(address) {
+                    ) : I2CDevice("MultiGasSensor"), manInst(&man), addr(address), gas(&Wire, address) {
                         module_address = address;
 
                         // Register the module with the manager
@@ -41,43 +42,42 @@ uint8_t Loom_MultiGasSensor::get_gas_i2c(const std::string& gasType) {
 void Loom_MultiGasSensor::initialize() {
     FUNCTION_START;
 
+    LOG(F("Scanning I2C for MultiGasSensor..."));
+    addr = scanI2C();
     gas.setI2cAddr(addr);
-    delay(100);
+    module_address = addr;
+    LOG(F("I2C address set"));
 
-    // If scan is necessary
-    if (!checkDeviceConnection()){
-        LOG(F("Scanning I2C for MultiGasSensor..."));
-        addr = scanI2C();
-        gas.setI2cAddr(addr);
-        module_address = addr;
-        needsReinit = false;
-        char *msg = (char*)malloc(100);
-        snprintf_P(msg, 100, PSTR("MultiGasSensor addr changed to: 0x%02X"), addr);
-        LOG(msg);
-        free(msg);
-    }
-    else{
-        LOG(F("MultiGasSensor connection found, no scan necessary!"));
-    }
 
     int retries = 0;
+    moduleInitialized = true;
     while (!gas.begin()){
-        ERROR(F("Failed to initialize Gas Sensor! Retrying..."));
+        LOG(F("Failed to initialize Gas Sensor! Retrying..."));
         moduleInitialized = false;
         delay(1000);
         retries++;
         if(retries > DF_MAX_RETRIES){
-            ERROR(F("Failed to initialize Gas Sensor... Check connections and try again!"));
+            LOG(F("Failed to initialize Gas Sensor... Check connections and try again!"));
             break;
         }
         moduleInitialized = true;
     }
+    LOG(F("Gas sensor initialized"));
 
     if(moduleInitialized){
-        gas.setTempCompensation(gas.ON);
-        gas.changeAcquireMode(gas.PASSIVITY);
+        LOG(F("Setting acquire mode"));
+        gas.changeAcquireMode(gas.INITIATIVE);
         delay(1000);
+        LOG(F("Acquire mode set to PASSIVITY"));
+
+        LOG(F("Setting temp compensation"));
+        gas.setTempCompensation(gas.OFF);
+        LOG(F("Temp compensation set to OFF"));
+
         LOG(F("Successfully initialized Gas Sensor!"));
+    }
+    else{
+        LOG(F("Gas sensor: Failed to initialize"));
     }
 
     FUNCTION_END;
@@ -88,6 +88,7 @@ void Loom_MultiGasSensor::initialize() {
 void Loom_MultiGasSensor::measure() {
     FUNCTION_START;
     if(moduleInitialized){
+        LOG(F("Checking device connection"));
         bool connectionStatus = checkDeviceConnection();
         if(connectionStatus && needsReinit){
             initialize();
@@ -108,6 +109,28 @@ void Loom_MultiGasSensor::measure() {
             gasData = new std::unordered_map<std::string, float>;
         }
 
+        if (gas.dataIsAvailable()){
+            LOG(F("Gas data available"));
+        }
+        else{
+            LOG(F("No gas data available"));
+        }
+        // String arduinoGasType = gas.queryGasType();
+        // std::string gasType = arduinoGasType.c_str();
+        // float data = gas.readGasConcentrationPPM();
+        // char *msg = (char*)malloc(100);
+        // snprintf_P(msg, 100, PSTR("Gas type: %s, data: %f"), gasType.c_str(), data);
+        // LOG(msg);
+        // free(msg);
+
+        // if(gas.dataIsAvailable()){
+        //     char *msg = (char*)malloc(100);
+        //     float data = AllDataAnalysis.gasconcentration;
+        //     snprintf_P(msg, 100, PSTR("Gas data available: %f"), data);
+        //     LOG(msg);
+        //     free(msg);
+        // }
+
         String queryResult;
         for(const auto& type : gasTypes) {
             (*gasData)[type] = 0.0;
@@ -126,6 +149,9 @@ void Loom_MultiGasSensor::measure() {
         float temp = gas.readTempC();
         (*gasData)["Temperature(C)"] = temp;
     }
+    else{
+        LOG(F("Gas sensor: Module not initialized"));
+    }
     FUNCTION_END;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -136,7 +162,7 @@ void Loom_MultiGasSensor::package() {
     if(moduleInitialized && gasData != nullptr){
         JsonObject json = manInst->get_data_object(getModuleName());
         for (auto i = gasData->begin(); i != gasData->end(); ++i){
-            json[i->first] = i->second;
+            json[i->first] = String(i->second);
         }
     }
     FUNCTION_END;
