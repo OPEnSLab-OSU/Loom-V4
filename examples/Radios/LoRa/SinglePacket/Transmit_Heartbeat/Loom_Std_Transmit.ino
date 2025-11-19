628f7f277180e21e802520916d3507f6742dc728
Duncan Everson <duncan.everson@icloud.com>
2025-11-19 02:11:44 -0800

    made it so example for std transmit of heartbeat converts time span to total seconds, not just grabbing the seconds from the timespan

--- examples/Radios/LoRa/SinglePacket/Transmit_Heartbeat/Loom_Std_Transmit.ino
+++ examples/Radios/LoRa/SinglePacket/Transmit_Heartbeat/Loom_Std_Transmit.ino
@@ -38,7 +38,7 @@ void loop() {
     lora.send(0);
   }
 
-  int milliseconds = lora.hbNextEvent().seconds() * 1000;
+  int milliseconds = lora.hbNextEvent().totalseconds() * 1000;
   Serial.print("Sleep for: ");
   Serial.print(String(milliseconds));
   Serial.println("");
