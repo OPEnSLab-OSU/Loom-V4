d065ec706ba1760af1679d248b6d6e49705bafb3
Your Name <you@example.com>
2025-11-21 12:27:44 -0800

    Remove credential files, updated .gitignore, and added README's

--- examples/Internet/Logging/WiFiMongoDBBatch/arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-
-#define SECRET_BROKER ""
-#define SECRET_PORT 1883
-#define DATABASE ""
-#define BROKER_USER ""
-#define BROKER_PASS ""
-#define PROJECT ""
