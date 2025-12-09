aed8faf2c34440930fd70bc381285bb13f0de286
RoryLGroom <groomro@oregonstate.edu>
2025-12-09 15:09:01 -0800

    Added better comments for multiplexer constructor

--- examples/Sensors/I2C/MultiplexerSD/MultiplexerSD.ino
+++ examples/Sensors/I2C/MultiplexerSD/MultiplexerSD.ino
@@ -41,7 +41,7 @@ Manager manager("Device", 1);
 //Loom_Hypnos(Manager& man, HYPNOS_VERSION version, TIME_ZONE zone, bool use_custom_time = false, bool useSD = true)
 Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST);
 
-// pass in manager, hypnos, and filename
+// pass in manager, hypnos for pulling file from SD card, and filename
 Loom_Multiplexer mux(manager, hypnos, "custom_addresses.json");
 
 
