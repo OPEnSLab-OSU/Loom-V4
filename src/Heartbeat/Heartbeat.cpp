2c35480cabdcaac553f5c51559a8f45cf85c1c8f
RoryLGroom <groomro@oregonstate.edu>
2026-01-11 15:33:34 -0800

    Added a folder for heartbeat in source and added methods from Lora heartbeat. Defined a constructor but will need more arguments. Will want to seperate LoRa specific functions into different conditional blocks to allow for usability with other connection methods.

---
+++ src/Heartbeat/Heartbeat.cpp
@@ -0,0 +1,261 @@
+#include "Heartbeat.h"
+#include "../Logger.h"
+
+
+
+
+
+void Loom_Heartbeat::heartbeatInit( const uint8_t newAddress, 
+                    const uint32_t pHeartbeatInterval, 
+                    const uint32_t pNormalWorkInterval,
+                    Loom_Hypnos* hypnosInstance)
+{
+    if(hypnosInstance != nullptr && pHeartbeatInterval < 60) {
+        WARNING(F("Heartbeat interval too low for Hypnos, setting to minimum of 60 seconds"));
+        heartbeatInterval_s = 60;
+    }
+    else if(pHeartbeatInterval < 5) {
+        WARNING(F("Heartbeat interval too low, setting to minimum of 5 seconds"));
+        heartbeatInterval_s = 5;
+    }
+    else 
+        heartbeatInterval_s = pHeartbeatInterval;
+
+    if(pNormalWorkInterval < 5) {
+        WARNING(F("Normal work interval too low, setting to minimum of 5 seconds"));
+        normWorkInterval_s = 5;
+    }
+    else 
+        normWorkInterval_s = pNormalWorkInterval;
+
+    heartbeatDestAddress = newAddress;
+
+    heartbeatTimer_s = heartbeatInterval_s;
+    normWorkTimer_s = normWorkInterval_s;
+    hypnosPtr = hypnosInstance;
+
+    if(hypnosPtr != nullptr) {
+        hypnosPtr->clearAlarms();
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TimeSpan Loom_Heartbeat::secondsToTimeSpan(const uint32_t totalSeconds) {
+    // Build from d/h/m/s:
+    uint16_t rem = 0;
+    uint16_t days = totalSeconds / 86400;
+    rem  = totalSeconds % 86400;
+    uint8_t hours = rem / 3600;
+    rem = rem % 3600;
+    uint8_t minutes = rem / 60;
+    uint8_t seconds = rem % 60;
+
+    return TimeSpan(days, hours, minutes, seconds);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+TimeSpan Loom_Heartbeat::calculateNextEvent() {
+    uint32_t secondsToWait = 0;
+    if(heartbeatTimer_s > 0 && normWorkTimer_s > 0) // check if heartbeat was initialized
+    {
+        if(heartbeatTimer_s < normWorkTimer_s) {
+            secondsToWait = heartbeatTimer_s;
+
+            if(normWorkTimer_s - secondsToWait > 0)
+                normWorkTimer_s = normWorkTimer_s - secondsToWait;
+            else
+                normWorkTimer_s = 5;
+
+            heartbeatTimer_s = heartbeatInterval_s;
+            heartbeatFlag = true;
+        }
+        else {
+            secondsToWait = normWorkTimer_s;
+
+            if (heartbeatTimer_s - secondsToWait > 0)
+                heartbeatTimer_s = heartbeatTimer_s - secondsToWait;
+            else
+                heartbeatTimer_s = 5;
+
+            normWorkTimer_s = normWorkInterval_s;
+            heartbeatFlag = false;
+        }
+
+        if (secondsToWait < 5)
+            secondsToWait = 5; // minimum wait time of 5 seconds for safety/stability.
+    }
+    return secondsToTimeSpan(secondsToWait);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Loom_Heartbeat::sendHeartbeat() {
+
+    // this is 200 because it is safely within the P2P and LoRaWAN limits for maximum size.
+    const uint16_t JSON_HEARTBEAT_BUFFER_SIZE = 200;
+
+    StaticJsonDocument<JSON_HEARTBEAT_BUFFER_SIZE> heartbeatDoc;
+    heartbeatDoc["type"] = "LoRa_heartbeat";
+    heartbeatDoc.createNestedArray("contents");
+
+    JsonObject objNestedId = heartbeatDoc.createNestedObject("id");
+    objNestedId["name"] = manager->get_device_name();
+    objNestedId["instance"] = manager->get_instance_num();
+
+    heartbeatDoc["battery_voltage"] = Loom_Analog::getBatteryVoltage();
+
+    if(hypnosPtr != nullptr) {
+        char utcTimeStr[21];
+        char localTimeStr[21];
+        DateTime utcTime = hypnosPtr->getCurrentTime();
+        DateTime localTime = hypnosPtr->getLocalTime(utcTime);
+        hypnosPtr->dateTime_toString(utcTime, utcTimeStr);
+        hypnosPtr->dateTime_toString(localTime, localTimeStr, true); // set third arg to true for local time format
+
+        JsonObject objNestedTimestamp = heartbeatDoc.createNestedObject("timestamp");
+        objNestedTimestamp["time_utc"] = utcTimeStr;
+        objNestedTimestamp["time_local"] = localTimeStr;
+    }
+
+    return send(heartbeatDestAddress, heartbeatDoc.as<JsonObject>());
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void Loom_Heartbeat::ensureHeartbeatHypnosAlarmsActive() {
+    if(hypnosPtr == nullptr) {
+        WARNING(F("Hypnos pointer not set - cannot ensure heartbeat alarm is active"));
+        return;
+    }
+
+    Serial.println("checking the value of the fired alarms bitmask:  @@@@@@@@@@@@@@");
+    Serial.println(hypnosPtr->getFiredAlarmsBM());
+
+    // checks if the alarms fired.
+    uint8_t firedAlarmsBitMask = hypnosPtr->getFiredAlarmsBM();
+    bool setAlarm1 = firedAlarmsBitMask & BM_ALARM_1;
+    bool setAlarm2 = firedAlarmsBitMask & BM_ALARM_2;
+    hypnosPtr->clearFiredAlarmsBM();
+
+    // check if alarm registers are cleared. Any cleared registers need to be re-set with the appropriate intervals.
+    if(hypnosPtr->isAlarm1Cleared())
+        setAlarm1 = true;
+    if (hypnosPtr->isAlarm2Cleared())
+        setAlarm2 = true;
+
+    uint32_t alarmOneTime = 0;
+    uint32_t alarmTwoTime = 0;
+
+    if (!setAlarm1)
+        alarmOneTime = hypnosPtr->getAlarmDate(1).unixtime();
+    if (!setAlarm2)
+        alarmTwoTime = hypnosPtr->getAlarmDate(2).unixtime();
+    uint32_t currentTime = hypnosPtr->getCurrentTime().unixtime();
+
+
+    // if both alarms need to be set, just set them both and return.
+    if(setAlarm1 && setAlarm2) {
+        hypnosPtr->clearAlarms();
+
+        TimeSpan timeToSetWith = secondsToTimeSpan(normWorkInterval_s);
+        hypnosPtr->setInterruptDuration(timeToSetWith);
+
+        timeToSetWith = secondsToTimeSpan(heartbeatInterval_s);
+        hypnosPtr->setSecondAlarmInterruptDuration(timeToSetWith);
+        Serial.println("Both alarms reset");
+        return;
+    }
+    else if(setAlarm1){
+        // sets alarm one, and resets alarm two if alarm 1 would overlap with it.
+        if (!setAlarm2 &&
+            alarmTwoTime != 0 &&
+            currentTime + normWorkInterval_s > alarmTwoTime - 5 &&
+            currentTime + normWorkInterval_s < alarmTwoTime + 5) {
+
+            uint32_t remainingSecondsAlarmTwo =
+                (alarmTwoTime > currentTime) ? (alarmTwoTime - currentTime) : 0;
+
+            hypnosPtr->clearAlarms();
+
+            TimeSpan ts = secondsToTimeSpan(normWorkInterval_s);
+            hypnosPtr->setInterruptDuration(ts);
+
+            ts = secondsToTimeSpan(heartbeatInterval_s + remainingSecondsAlarmTwo);
+            hypnosPtr->setSecondAlarmInterruptDuration(ts);
+
+            ERROR("Skipping heartbeat since normal work will overlap");
+            return;
+        }
+        // sets normal work alarm if no overlap detected.
+        else {
+            TimeSpan ts = secondsToTimeSpan(normWorkInterval_s);
+            hypnosPtr->setInterruptDuration(ts);
+            ERROR("Alarm set for normal work interval");
+        }
+    }
+    else if(setAlarm2){
+        // skips heartbeat alarm if it would overlap with normal work alarm.
+        if (!setAlarm1 &&
+            alarmOneTime != 0 &&
+            currentTime + heartbeatInterval_s > alarmOneTime - 5 &&
+            currentTime + heartbeatInterval_s < alarmOneTime + 5) {
+
+            ERROR("Skipping heartbeat alarm to avoid conflict with normal work alarm");
+            return;
+        }
+        // sets heartbeat alarm if no overlap detected.
+        else {
+            TimeSpan ts = secondsToTimeSpan(heartbeatInterval_s);
+            hypnosPtr->setSecondAlarmInterruptDuration(ts);
+            Serial.println("Alarm set for heartbeat interval");
+        }
+    }
+    else {
+        ERROR("No alarms were set - nothing to do");
+        return;
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void Loom_Heartbeat::adjustHbFlagFromAlarms() {
+    if(hypnosPtr == nullptr) {
+        WARNING(F("Hypnos pointer not set - cannot adjust heartbeat flag based on alarms"));
+        return;
+    }
+
+    bool alarm1Fired = hypnosPtr->alarm1Fired();
+    bool alarm2Fired = hypnosPtr->alarm2Fired();
+
+    ERROR("Triggered Alarm Number: ");
+    if(alarm1Fired && alarm2Fired)
+        Serial.println("3");
+    else if(alarm2Fired)
+        Serial.println("2");
+    else if(alarm1Fired)
+        Serial.println("1");
+    else if(!alarm1Fired && !alarm2Fired)
+        Serial.println("0");
+    else
+        Serial.println("Error determining alarm number");
+
+    if(alarm1Fired && alarm2Fired) {
+        Serial.println("Both alarms have fired - defaulting to alarm 1 behavior");
+        setHeartbeatFlag(false);
+        return;
+    }
+
+    if(alarm1Fired) {
+        setHeartbeatFlag(false); 
+        Serial.println("Adjusted heartbeat flag from alarm 1");
+        return;
+    }
+    else if (alarm2Fired) {
+        setHeartbeatFlag(true);
+        Serial.println("Adjusted heartbeat flag from alarm 2");
+        return;
+    }
+
+    ERROR("No alarms have fired - cannot adjust heartbeat flag");
+    return;
+}
---
+++ src/Heartbeat/Heartbeat.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "../Loom_Manager.h"
+#include "../Module.h"
+#include "../Loom_Hypnos/Loom_Hypnos.h"
+
+#include "../Radio/Loom_LoRa.h"
+#include "../Internet/Connectivity/Loom_LTE.h"
+#include "../Internet/Connectivity/Loom_Wifi"
+#include "../Radio/Loom_Freewave/Loom_Freewave.h"
+
+enum class ConnectionType {
+    Radio, 
+    Freewave,
+    LTE,
+    Wifi
+}
+
+class Loom_Heartbeat : public Module {
+    public:
+    /**
+     * @brief Construct a new Loom_Heartbeat object
+     * 
+     * @param man Manager instance
+     * @param connectionMethod Radio, Freewave, LTE, or Wifi module
+     */
+    Loom_Heartbeat(Manager& man, Module& connectionMethod);
+
+    ~Loom_Heartbeat();
+
+    /**
+     * Initialize heartbeat variables with specified parameters
+     * 
+     * @param newAddress Destination address for heartbeats
+     * @param heartbeatInterval Interval between heartbeats (scalar)
+     * @param normalWorkInterval Interval between normal work cycles (scalar)
+     * @param hypnosInstance Pointer to the Hypnos instance being used.
+     * 
+     * @note heartbeatInterval and normalWorkInterval must be seconds.
+     */
+    void heartbeatInit( const uint8_t newAddress, 
+                        const uint32_t pHeartbeatInterval,
+                        const uint32_t pNormalWorkInterval,
+                        Loom_Hypnos* hypnosInstance = nullptr);
+
+    /**
+     * Convert seconds to a TimeSpan object
+     * 
+     * @param secondsToConvert The number of seconds to convert
+     */ 
+    TimeSpan secondsToTimeSpan(const uint32_t secondsToConvert);
+
+    /**
+     * when heartbeat has been initialized, find the time until the next wake up / upnause event 
+     * to either do heartbeat or normal logic.
+     * 
+     * @return time until next event in the same unit as heartbeatInit parameters. 
+     * 
+     * @note enforces a minimum wait time of 5 seconds to ensure stability/safety. This is because
+     *      very short sleep times can cause undefined behavior with firmware/hardware systems like 
+     *      sleep/interrupt overhead, Serial/LoRa Cleanup, RTC rounding, Power Rails and Peripheral
+     *      power down/up times. 
+     *      Intervals will still be followed, after the minimum 5 second wait.
+     */
+    TimeSpan calculateNextEvent();
+
+    /**
+     * Set the heartbeat flag bool
+     * 
+     * @param flag The value to set the heartbeat flag to
+     */
+    void setHeartbeatFlag(const bool flag) { heartbeatFlag = flag; };
+
+    /**
+     * Get the status of the heartbeat flag bool
+     */
+    bool getHeartbeatFlag() const { return heartbeatFlag; };
+
+    /**
+     * Send a heartbeat packet with basic device info
+     */
+    bool sendHeartbeat();
+
+    /**
+     * Ensure that the normal work alarm (1) and heartbeat alarm (2) are both set
+     */
+    void ensureHeartbeatHypnosAlarmsActive();
+
+    /**
+     * Adjust the heartbeat flag based on which alarm triggered
+     */
+    void adjustHbFlagFromAlarms();
+    
+    private:
+    
+    uint32_t heartbeatTimer_s = 0;
+    uint32_t heartbeatInterval_s = 0;
+    uint32_t normWorkTimer_s = 0;
+    uint32_t normWorkInterval_s = 0;
+
+    uint8_t heartbeatDestAddress = 0;
+
+    bool heartbeatFlag = false;
+    Loom_Hypnos* hypnosPtr = nullptr; // Pointer to the hypnos instance
+    
+
+
+
+
+}
