adb532306aa18364bdc223ce9c3c6fa173e932d8
Duncan Everson <duncan.everson@icloud.com>
2026-01-29 21:47:15 -0800

    got rid of the adapter abstractions as they were unnecessary in the wake of the newly implemented createJSONPaylod() method.

--- src/Heartbeat/Adapter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Adapter {
-    public:
-        virtual bool sendHeartbeat(const uint8_t address, const JsonObject& payload) = 0;
-
-        // virtual bool sendHeartbeat(const char* metadata) = 0;
-};
