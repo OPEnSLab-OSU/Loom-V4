c78c082b8621958ba72e48c97b9530a121d91aa0
RoryLGroom <groomro@oregonstate.edu>
2026-02-05 20:09:27 -0800

    Runs and flashes to board, only problem is that we get the message that the broker is not set correctly. This is done in MQTTComponent.cpp in  connectToBroker function, and will not send to mongo. Everything else - heartbeat, initalization, seems to be working correctly. Will need to be tested more for troubleshooting. Message me for the heartbeat.json file to recreate

--- examples/Heartbeat/MongoDB/MongoDB_Transmit_Heartbeat.ino
+++ examples/Heartbeat/MongoDB/MongoDB_Transmit_Heartbeat.ino
@@ -6,13 +6,14 @@
 #include <Loom_Manager.h>
 
 #include <Heartbeat/Loom_Heartbeat.h>
+#include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
 
 #include <Internet/Connectivity/Loom_LTE/Loom_LTE.h>
 #include <Internet/Logging/Loom_MongoDB/Loom_MongoDB.h>
 
 Manager manager("Device", 1);
-
-Loom_LTE lte(manager);
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, true);
+Loom_LTE lte(manager, "hologram", "", "");
 Loom_MongoDB mqtt(manager, lte);
 
 // heartbeat instantiation
@@ -20,11 +21,35 @@ uint32_t hbInterval_s = 15;
 uint32_t normalInterval_s = 35;
 Loom_Heartbeat heartbeat(hbInterval_s, normalInterval_s, &manager);
 
+void isrTrigger()
+{
+  hypnos.wakeup();
+}
+
 void setup() {
   manager.beginSerial();
-  manager.initialize();
+    // Both power rails should be on when awake
+  hypnos.setWakeConfiguration(POWERRAIL_CONFIG::PR_3V_ON_5V_ON);
+
+  // Only the 5V rail should be on during sleep
+  hypnos.setSleepConfiguration(POWERRAIL_CONFIG::PR_3V_OFF_5V_ON);
+
+  // Enable the hypnos rails
+  hypnos.enable();
+
+  //Time Sync Using LTE 
+  hypnos.setNetworkInterface(&lte);
+
   //will need to pull mqtt creds from sd card
-  mqtt.loadConfigFromJSON("name.csv");
+  mqtt.loadConfigFromJSON(hypnos.readFile("heartbeat.json"));
+
+  manager.initialize();
+
+  // Register the ISR and attach to the interrupt
+  hypnos.registerInterrupt(isrTrigger);
+
+  hypnos.networkTimeUpdate();
+
 }
 
 void loop() {
@@ -33,8 +58,10 @@ void loop() {
   {
     char buffer[MAX_JSON_SIZE];
     Serial.println("Within Heartbeat Branch");
-    JsonObject payload = heartbeat.createJSONPayload();
-    serializeJson(payload, buffer);
+    const uint16_t JSON_HEARTBEAT_BUFFER_SIZE = 200;
+    StaticJsonDocument<JSON_HEARTBEAT_BUFFER_SIZE> basePayload;
+    heartbeat.createJSONPayload(basePayload);
+    serializeJson(basePayload, buffer);
     mqtt.publishMetadata(buffer);
   }
   else {
