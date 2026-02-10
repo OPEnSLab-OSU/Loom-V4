8efd0ba9570560a6312ee9478fb1baed71fa48db
Duncan Everson <duncan.everson@icloud.com>
2026-02-09 20:17:25 -0800

    takes out the lora.h/.cpp file's heartbeat functions, as the heartbeat functions are working in Heartbeat.h/.cpp

--- src/Radio/Loom_LoRa/Loom_LoRa.h
+++ src/Radio/Loom_LoRa/Loom_LoRa.h
@@ -197,69 +197,6 @@ public:
      */ 
     bool receiveBatch(uint timeout, int *numberOfPackets, uint8_t *fromAddress);
 
-    /**
-     * Initialize heartbeat variables with specified parameters
-     * 
-     * @param newAddress Destination address for heartbeats
-     * @param heartbeatInterval Interval between heartbeats (scalar)
-     * @param normalWorkInterval Interval between normal work cycles (scalar)
-     * @param hypnosInstance Pointer to the Hypnos instance being used.
-     * 
-     * @note heartbeatInterval and normalWorkInterval must be seconds.
-     */
-    void heartbeatInit( const uint8_t newAddress, 
-                        const uint32_t pHeartbeatInterval,
-                        const uint32_t pNormalWorkInterval,
-                        Loom_Hypnos* hypnosInstance = nullptr);
-
-    /**
-     * Convert seconds to a TimeSpan object
-     * 
-     * @param secondsToConvert The number of seconds to convert
-     */ 
-    TimeSpan secondsToTimeSpan(const uint32_t secondsToConvert);
-
-    /**
-     * when heartbeat has been initialized, find the time until the next wake up / upnause event 
-     * to either do heartbeat or normal logic.
-     * 
-     * @return time until next event in the same unit as heartbeatInit parameters. 
-     * 
-     * @note enforces a minimum wait time of 5 seconds to ensure stability/safety. This is because
-     *      very short sleep times can cause undefined behavior with firmware/hardware systems like 
-     *      sleep/interrupt overhead, Serial/LoRa Cleanup, RTC rounding, Power Rails and Peripheral
-     *      power down/up times. 
-     *      Intervals will still be followed, after the minimum 5 second wait.
-     */
-    TimeSpan calculateNextEvent();
-
-    /**
-     * Set the heartbeat flag bool
-     * 
-     * @param flag The value to set the heartbeat flag to
-     */
-    void setHeartbeatFlag(const bool flag) { heartbeatFlag = flag; };
-
-    /**
-     * Get the status of the heartbeat flag bool
-     */
-    bool getHeartbeatFlag() const { return heartbeatFlag; };
-
-    /**
-     * Send a heartbeat packet with basic device info
-     */
-    bool sendHeartbeat();
-
-    /**
-     * Ensure that the normal work alarm (1) and heartbeat alarm (2) are both set
-     */
-    void ensureHeartbeatHypnosAlarmsActive();
-
-    /**
-     * Adjust the heartbeat flag based on which alarm triggered
-     */
-    void adjustHbFlagFromAlarms();
-
 private:
     // receives some data from lora
     bool receiveFromLoRa(uint8_t *buf, uint8_t buf_size, uint timeout, 
@@ -291,17 +228,6 @@ private:
     Loom_BatchSD *batchSD = nullptr;   // Pointer to the batchSD
     
     bool poweredUp = true;
-
-    // maximum time for uint32_t is 4294967295, which is ~136 years in seconds.
-    uint32_t heartbeatTimer_s = 0;
-    uint32_t heartbeatInterval_s = 0;
-    uint32_t normWorkTimer_s = 0;
-    uint32_t normWorkInterval_s = 0;
-
-    uint8_t heartbeatDestAddress = 0;
-
-    bool heartbeatFlag = false;
-    Loom_Hypnos* hypnosPtr = nullptr; // Pointer to the hypnos instance
 
     uint8_t deviceAddress;      // Device address
     int16_t signalStrength;     // Strength of the signal received
