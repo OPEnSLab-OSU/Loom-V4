7d90d374f4a7f8b725186c6a1d91e57371de14d5
ReidP-Source <reidpettibone@gmail.com>
2026-02-13 13:09:53 -0800

    Fixed incorrect manager declaration in vcheck example

--- examples/Hypnos/Hypnos_vCheck/Hypnos_vCheck.ino
+++ examples/Hypnos/Hypnos_vCheck/Hypnos_vCheck.ino
@@ -9,9 +9,9 @@
  #include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
 
 
-Manager man("Device", 1);
+Manager mananger("Device", 1);
 
-Loom_Hypnos hypnos(man, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, false, false);
+Loom_Hypnos hypnos(mananger, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, false, false);
 
 void setup(){
 ENABLE_SD_LOGGING;
@@ -29,7 +29,7 @@ void loop(){
 // Run voltage check with default args (voltage_min = 0.0, pin = A7, scale = 2.0)
 // Scale refers to the voltage divisor that reduces a higher voltage to a lower acceptable voltage. 
 // Vout = Vsource x Resistance2 / (Resistance1 + Resistance2)
-hypnos.checkVoltage(float vmin = 4.0f, int pin = A7, float scale = 2.0);
+hypnos.checkVoltage(4.0f, A7, 2.0);
 
 manager.pause(5000);
 }
