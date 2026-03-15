fa0e75c93c295979f40e0b1524e5e2a3902b5b95
Duncan Everson <duncan.everson@icloud.com>
2026-03-15 16:21:34 -0700

    fixed a compilation error relating to a recently added log statement.

--- src/Radio/Loom_LoRa/Loom_LoRa.cpp
+++ src/Radio/Loom_LoRa/Loom_LoRa.cpp
@@ -652,7 +652,7 @@ bool Loom_LoRa::send(const uint8_t destinationAddress) {
     }
 
     if(this->batchPacketsToSend == 0) {
-        LOG("Completed message sending process. Dropping any active Handshake.")
+        LOG("Completed message sending process. Dropping any active Handshake.");
         this->handshakeEstablished = false;
         this->activePartner = -1;
     }
