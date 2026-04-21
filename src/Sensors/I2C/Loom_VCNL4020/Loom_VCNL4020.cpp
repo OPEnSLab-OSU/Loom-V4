1d76c9870379357034bffdefd3a909dc4e913d84
shawn-singharaj <shawn.singharaj@gmail.com>
2026-04-21 13:50:09 -0700

    Fixed constructors, and added delays to fix ALS measurement bug, Loomified VCNL4020 tested and working

--- src/Sensors/I2C/Loom_VCNL4020/Loom_VCNL4020.cpp
+++ src/Sensors/I2C/Loom_VCNL4020/Loom_VCNL4020.cpp
@@ -1,15 +1,28 @@
 #include "Loom_VCNL4020.h"
 #include "Logger.h"
 
-Loom_VCNL4020::Loom_VCNL4020(Manager &man, int address, bool useMux, uint16_t ambRate,
-                                uint16_t ambAvg, uint16_t proxLED, uint16_t proxRate)
-    : I2CDevice("VCNL4020"), managerInstance(&man), vcnl(),  ambRate(ambRate), ambAvg(ambAvg), proxLED(proxLED), proxRate(proxRate), proxFreq(proxFreq) {
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constructor initialize config settings
+Loom_VCNL4020::Loom_VCNL4020(Manager &man, int address, bool useMux, vcnl4020_ambientrate ambRate,
+                                vcnl4020_averaging ambAvg, uint8_t proxLED, vcnl4020_proxrate proxRate, vcnl4020_proxfreq proxFreq)
+                            : I2CDevice("VCNL4020"), 
+                            managerInstance(&man), 
+                            vcnl(),  
+                            ambRate(ambRate), 
+                            ambAvg(ambAvg), 
+                            proxLED(proxLED), 
+                            proxRate(proxRate), 
+                            proxFreq(proxFreq) 
+    {
         module_address = address;
 
         if(!useMux){
             managerInstance->registerModule(this);
         }
     }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Loom_VCNL4020::initialize(){
     FUNCTION_START;
@@ -18,38 +31,58 @@ void Loom_VCNL4020::initialize(){
         moduleInitialized = false;
     } else {
         LOG(F("Successfully initialized module VCNL4020!"));
+        delay(250); // Give sensor time to produce a reading
     }
     FUNCTION_END;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Loom_VCNL4020::measure(){
     FUNCTION_START;
-    printf("VCNL4020 Measure\n");
+    Serial.println("VCNL4020 Measure\n");
     if(moduleInitialized) {
         bool connectionStatus = checkDeviceConnection();
 
-        if(connectionStatus && needsRenit){
+        // If need to reinitialize
+        if(connectionStatus && needsReinit){
             initialize();
             needsReinit = false;
         }
 
+        // If no connection
         else if(!connectionStatus){
-            ERROR("No acknoledge recieved from VCNL4020 module")
+            ERROR("No acknoledge recieved from VCNL4020 module");
             FUNCTION_END;
             return;
         }
+
+        // When everything works, measure
         LOG("Running Adafruit VCNL4020 measure functions");
+    
+        // make sure ALS works
+        uint32_t start = millis();
+        while (!vcnl.isAmbientReady()) {
+            if (millis() - start > 100) {
+                ERROR("Ambient timeout");
+                return;
+            }
+        }
+
+        // Measure ambient light and proximity
         ambientLight = vcnl.readAmbient();
         proximity = vcnl.readProximity();
 
     }
     FUNCTION_END;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Loom_VCNL4020::package(){
     FUNCTION_START;
     if(moduleInitialized) {
-        JsonObject json = manInst->get_data_object(getModuleName());
+        JsonObject json = managerInstance->get_data_object(getModuleName());
         // higher bit values indicate more light
         json["Ambient Light_counts"] = ambientLight;
         json["Proximity"] = proximity;
@@ -58,20 +91,23 @@ void Loom_VCNL4020::package(){
     Serial.println(proximity);
     FUNCTION_END;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Loom_VCNL4020::power_up(){
     FUNCTION_START;
     if(moduleInitialized){
-        
+        // If sensor does not reconnect upon wakeup
         if (!checkDeviceConnection()) {
             ERROR("VCNL4020 not responding after wake");
             FUNCTION_END;
+            return;
         }
 
         // Mimic adafruit VCNL4020 begin() function calls to reconfigure settings upon power on
         vcnl.enable(false, false, false);
 
-        vcnl.setOnDemand(false);
+        vcnl.setOnDemand(false, false);
         vcnl.setProxRate(proxRate);
         vcnl.setProxLEDmA(proxLED);
         vcnl.setAmbientRate(ambRate);
@@ -82,12 +118,8 @@ void Loom_VCNL4020::power_up(){
         
         vcnl.enable(true, true, true);
 
-        delay(10);
+        delay(250); // sensor needs time to produce a reading
     }
     FUNCTION_END;
 }
-                               
-
-
-
-                                
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/Sensors/I2C/Loom_VCNL4020/Loom_VCNL4020.h
+++ src/Sensors/I2C/Loom_VCNL4020/Loom_VCNL4020.h
@@ -13,11 +13,12 @@
  * Proximity =  16-bit counts (ADC output)
  * Ambient Light = 16-bit ALS measurement (ADC output)
  * 
- * Shawn Singharaj 
+ * @author Shawn Singharaj 
 */
 
-class Loom_VCNL4020 :: public I2CDevice {
+class Loom_VCNL4020 : public I2CDevice {
     protected:
+        // Manager lifecycle functions
         void initialize() override;
         void measure() override;
         void package() override;
@@ -25,26 +26,37 @@ class Loom_VCNL4020 :: public I2CDevice {
         void power_down() override {};
 
     public:
+        /**
+         * VCNL4020 Constructor
+         * @param managerInstance reference of the manager to run manager lifecycle functions
+         * @param address 0x13 by default
+         * @param ambRate, ambAvg, proxLED, proxRate, proxFreq are config setting for sensor upon manager.power_up()
+         */
         Loom_VCNL4020(Manager &managerInstance, int address = 0x13, bool useMux = false,
-                        ambRate = AMBIENT_RATE_10_SPS, ambAvg = AVG_1_SAMPLES,
-                        proxLED = 200, proxRate = PROX_RATE_250_PER_S, proxFreq = PROX_FREQ_390_625_KHZ);
+                        vcnl4020_ambientrate ambRate = AMBIENT_RATE_10_SPS, 
+                        vcnl4020_averaging ambAvg = AVG_1_SAMPLES,
+                        uint8_t proxLED = 200, 
+                        vcnl4020_proxrate proxRate = PROX_RATE_250_PER_S, 
+                        vcnl4020_proxfreq proxFreq = PROX_FREQ_390_625_KHZ);
 
+        // Read the ambient light level
         uint16_t readAmbient() { return ambientLight; }
 
+        // Read the proximity level
         uint16_t readProximity() { return proximity; }
 
     private:
         Manager *managerInstance;           // Manager instance
         Adafruit_VCNL4020 vcnl;             // Adafruit's VCNL4020 object
-        uint16_t ambientLight;              // Lux illumination value
-        uint16_t proximity;
+        uint16_t ambientLight;              // Ambient illumination value
+        uint16_t proximity;                 // Proximity value
 
-        uint8_t ambRate;
-        uint8_t ambAvg;
-        uint8_t proxRate;
+        // Config settings for power_up
+        vcnl4020_ambientrate ambRate;
+        vcnl4020_averaging ambAvg;
+        vcnl4020_proxrate proxRate;
         uint8_t proxLED;
-        uint8_t proxFreq;
+        vcnl4020_proxfreq proxFreq;
 
         bool initialized = true; // True until set to false
-
-}
+};
