c0bff8a14b3bf4006ea211aea22ab4d31f03cc10
keahi <trenstonricks@gmail.com>
2026-05-06 19:52:42 -0700

    first commit

--- src/Loom_Manager.h
+++ src/Loom_Manager.h
@@ -22,7 +22,7 @@ class Manager {
      * @param devName Device name to provided for logging purposes
      * @param instanceNum Instance number for logging purposes
      */
-    Manager(const char *devName, uint32_t instanceNum);
+    Manager(const char *devName, uint32_t instanceNum, uint32_t nw_interval);
 
     /**
      * Registers a new sub-module to be controlled by the manager (Used on sensors so measure and
@@ -71,6 +71,7 @@ class Manager {
      *  Calls the package function to store all data from those sensors into a nice JSON package
      */
     void package();
+    void package(bool heartbeat);
 
     /**
      *  Calls the power_up function on each module to re-init after sleep
@@ -172,4 +173,8 @@ class Manager {
     bool usingHypnos = false;    // If the setup is using a hypnos
     bool hypnosEnabled = false; // If the power rails on the hypnos are enabled this means we should
                                 // be able to initialize
+
+    /* Heartbeat Interval Variables */
+        uint32_t normalWorkInterval = 0;
+        uint32_t intervalCount = 0;
 };
