88e2b31ca187ba3c1039080fc0a0fd563906548e
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2026-06-22 20:17:37 -0700

    moire sketches, tweaked flash cfg

--- examples/Lab Examples/Jolteon/SAMD21J18_Blinky/build/arduino.samd.arduino_zero_native/flash_j18.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-openocd -f flash_j18.cfg
-pause
--- examples/Lab Examples/Jolteon/SAMD21J18_Blinky/build/arduino.samd.arduino_zero_native/flash_j18.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location $ScriptDir
-
-openocd -f .\flash_j18.cfg
