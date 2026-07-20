059ec9014da316d11d304d11d5ab4eb5b362ef94
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2026-07-20 14:30:28 -0700

    updated jolteon test examples

--- examples/Lab Examples/Jolteon/Loomified_LTE_R5_example/Loomified_LTE_R5_example.ino
+++ examples/Lab Examples/Jolteon/Loomified_LTE_R5_example/Loomified_LTE_R5_example.ino
@@ -3,24 +3,45 @@
  *
  * MANAGER MUST BE INCLUDED FIRST IN ALL CODE
  */
-#include "arduino_secrets.h"
 
 #include <Loom_Manager.h>
-
-// Loom Modules
+#include "arduino_secrets.h"
 #include <Internet/Connectivity/Loom_LTE/Loom_LTE.h>
+
+static constexpr uint8_t LTE_SUPPLY_RAIL_ENABLE_PIN = 6;
+static constexpr uint32_t LTE_SUPPLY_STARTUP_DELAY_MS = 1000;
 
 Manager manager("Device", 1);
 
-Loom_LTE lte(manager, NETWORK_NAME, NETWORK_USER, NETWORK_PASS, A5, OPENS, -1,
-             LTE_MODEM::SARA_R5);
+Loom_LTE lte(
+    manager,
+    NETWORK_NAME,
+    NETWORK_USER,
+    NETWORK_PASS,
+    A5,
+    OPENS,
+    -1,
+    LTE_MODEM::SARA_R5
+);
+
+static void enableLTESupplyRail() {
+    digitalWrite(LTE_SUPPLY_RAIL_ENABLE_PIN, HIGH);
+    pinMode(LTE_SUPPLY_RAIL_ENABLE_PIN, OUTPUT);
+    digitalWrite(LTE_SUPPLY_RAIL_ENABLE_PIN, HIGH);
+
+    delay(LTE_SUPPLY_STARTUP_DELAY_MS);
+}
 
 void setup() {
-  manager.beginSerial();
-  manager.initialize();
+    manager.beginSerial();
+
+    Serial.println(F("Enabling Jolteon LTE supply rail on GPIO6."));
+    enableLTESupplyRail();
+
+    manager.initialize();
 }
 
 void loop() {
-  lte.verifyConnection();
-  manager.pause(5000);
+    lte.verifyConnection();
+    manager.pause(5000);
 }
