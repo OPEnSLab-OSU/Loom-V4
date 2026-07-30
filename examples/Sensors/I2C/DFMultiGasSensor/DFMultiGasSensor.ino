0cabd371aa6b4e2525944faf14341c98978507fd
Campbell Carlberg <SirCampbell@10-249-100-35.wireless.oregonstate.edu>
2026-07-30 12:08:06 -0700

    Add comment clarification for MUX implication - DFRobot

--- examples/Sensors/I2C/DFMultiGasSensor/DFMultiGasSensor.ino
+++ examples/Sensors/I2C/DFMultiGasSensor/DFMultiGasSensor.ino
@@ -3,13 +3,17 @@
  */
 #include <Loom_Manager.h>
 #include <Sensors/I2C/Loom_DFMultiGasSensor/Loom_DFMultiGasSensor.h>
-
+#include <Hardware/Loom_Multiplexer/Loom_Multiplexer.h>
 
 // If the sensor is freezing on init try disconnecting the power and re-connecting it
 Manager manager("Device", 1);
 
 // MANAGER, I2C ADDRESS, INIT RETRY LIMIT, USE MUX
 Loom_DFMultiGasSensor gas(manager, 0x77, 10, false);
+
+// If using Multiplexer, use base addr 0x74
+// Loom_Multiplexer mux(manager, {0x74});
+
 
 void setup() {
   manager.beginSerial();
