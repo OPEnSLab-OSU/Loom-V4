4f26f990247cae9c16e40e19afc1d7a019563299
Quinn Yockey <qyockey@tutanota.de>
2026-08-05 09:54:58 -0700

    Use size_t for iteration

--- src/Sensors/Loom_Analog/Loom_Analog.cpp
+++ src/Sensors/Loom_Analog/Loom_Analog.cpp
@@ -4,7 +4,7 @@
 void Loom_Analog::measure() {
 
     // Read the data from the given analog pin
-    for (int i = 0; i < pinMappings.size(); i++) {
+    for (size_t i = 0; i < pinMappings.size(); i++) {
 
         /* If we are measuring the Vbat pin we want a little different behavior */
         if (pinMappings[i]->pinNumber == A7) {
@@ -28,14 +28,14 @@ void Loom_Analog::package() {
     JsonObject json = manInst->get_data_object(getModuleName());
 
     /* Loop over the list of pins and pull out the data to formulate the JSON entries*/
-    for (int i = 0; i < pinMappings.size(); i++) {
+    for (size_t i = 0; i < pinMappings.size(); i++) {
         memset(output, '\0', 10);
         json[pinMappings[i]->name] = pinMappings[i]->analog;
 
         /* Append MV to the name to differentiate between normal analog and the millivolt
          * representation */
-        strncat(output, pinMappings[i]->name, 10);
-        strncat(output, "_MV", 10);
+        strncat(output, pinMappings[i]->name, 10 - 1);
+        strncat(output, "_MV", 10 - 1);
         json[output] = pinMappings[i]->analog_mv;
     }
 }
@@ -70,7 +70,7 @@ float Loom_Analog::analogToMV(int analog) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 float Loom_Analog::getMV(int pin) {
-    for (int i = 0; i < pinMappings.size(); i++) {
+    for (size_t i = 0; i < pinMappings.size(); i++) {
         if (pinMappings[i]->pinNumber == pin) {
             return pinMappings[i]->analog_mv;
         }
@@ -80,7 +80,7 @@ float Loom_Analog::getMV(int pin) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 float Loom_Analog::getAnalog(int pin) {
-    for (int i = 0; i < pinMappings.size(); i++) {
+    for (size_t i = 0; i < pinMappings.size(); i++) {
         if (pinMappings[i]->pinNumber == pin) {
             return pinMappings[i]->analog;
         }
