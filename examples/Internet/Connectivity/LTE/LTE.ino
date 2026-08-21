08eedea116dfd9c4364b0c810d9590778de4b12c
Campbell Carlberg <carlberc@oregonstate.edu>
2026-08-21 11:09:41 -0700

    fixes for dev

--- examples/Internet/Connectivity/LTE/LTE.ino
+++ examples/Internet/Connectivity/LTE/LTE.ino
@@ -7,20 +7,30 @@
 
 #include <Loom_Manager.h>
 
-// Loom Modules
+#include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
+
 #include <Internet/Connectivity/Loom_LTE/Loom_LTE.h>
+
 
 Manager manager("Device", 1);
 
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIMEZONE::PST);
+
 Loom_LTE lte(manager, NETWORK_NAME, NETWORK_USER, NETWORK_PASS);
 
-void setup() {
 
+void setup() 
+{
   manager.beginSerial();
+
+  hypnos.enable();
+
   manager.initialize();
 }
 
-void loop() {
+void loop()
+{
   lte.verifyConnection();
+
   manager.pause(5000);
 }
--- examples/Internet/Connectivity/WIFI/WIFI.ino
+++ examples/Internet/Connectivity/WIFI/WIFI.ino
@@ -3,23 +3,33 @@
  * 
  * MANAGER MUST BE INCLUDED FIRST IN ALL CODE
  */
+
 #include "arduino_secrets.h"
-
 #include <Loom_Manager.h>
-
+#include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
 #include <Internet/Connectivity/Loom_Wifi/Loom_Wifi.h>
+
 
 Manager manager("Device", 1);
 
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST);
+
 Loom_WIFI wifi(manager, CommunicationMode::CLIENT, SECRET_SSID, SECRET_PASS);
 
-void setup() {
 
+void setup() 
+{
   manager.beginSerial();
+
+  hypnos.enable();
+
   manager.initialize();
 }
 
-void loop() {
+
+void loop() 
+{
   wifi.verifyConnection();
+
   manager.pause(5000);
 }
